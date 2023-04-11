9071dd4832788107ba489e4f8778bd088894ec94
Padraig Dooley <padraigdooley@gmail.com>
2023-04-11 20:01:20 +0100

    add product view and template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Product, Category
+
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/products.html', context)
--- vendor/models.py
+++ vendor/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 class Vendor(models.Model):
     name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.OneToOneField(User, related_name='vendor', on_delete=models.CASCADE)
 
     class Meta:
