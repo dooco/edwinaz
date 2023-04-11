9eefc634846f2fe9318401bfbe1083e17f6a8159
Padraig Dooley <padraigdooley@gmail.com>
2023-04-11 21:29:52 +0100

    add product view functionality

--- products/views.py
+++ products/views.py
@@ -14,5 +14,17 @@ def all_products(request):
     context = {
         'products': products,
     }
-    
+
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
