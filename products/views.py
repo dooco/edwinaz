5c2340ef6b6817c7be038fdd083ed9430f1eafb3
Padraig Dooley <padraigdooley@gmail.com>
2023-04-12 22:03:36 +0100

    add caregory filtering

--- products/views.py
+++ products/views.py
@@ -10,9 +10,28 @@ from .models import Product, Category
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+  
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               ("You didn't enter any search criteria!"))
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
