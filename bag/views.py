84305000b0a206102264006b3ec9d4da99024a3b
Padraig Dooley <padraigdooley@gmail.com>
2023-04-16 19:37:39 +0100

    add adjust & remove item from bag and calc_subtotal temoplate filter

--- bag/views.py
+++ bag/views.py
@@ -7,18 +7,19 @@ from products.models import Product
 
 
 def view_bag(request):
+
     """ A view to renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
+
     """ Add a quantity of the specified product to the shopping bag """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    
 
     bag = request.session.get('bag', {})
 
@@ -34,3 +35,47 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
 
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+
+    """ Adjust the quantity of the specified product to the shopping bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request,
+                            (f'Updated {product.name} '
+                            f'quantity to {bag[item_id]}'))
+    else:
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} to your bag')
+
+    request.session['bag'] = bag
+
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+
+    """ Remove the specified product to the shopping bag """
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} to your bag')
+
+        request.session['bag'] = bag
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
