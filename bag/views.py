e9e40b881331213947bfd7b54329e00bb450620c
Padraig Dooley <padraigdooley@gmail.com>
2023-04-17 08:04:21 +0100

    add checkout app and models

--- bag/views.py
+++ bag/views.py
@@ -77,5 +77,6 @@ def remove_from_bag(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error resolving item: {e}')
         return HttpResponse(status=500)
 
