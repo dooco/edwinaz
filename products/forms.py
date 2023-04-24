aab87d5fa1317ebcc75c662140ad1cc5eb97bb17
Padraig Dooley <padraigdooley@gmail.com>
2023-04-24 07:27:44 +0100

    add product add, form, product_add.html, update base.html

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 # from .widgets import CustomClearableFileInput
 from .models import Product, Category
+# from django.forms.widgets import ClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -9,10 +10,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image',
-                             required=False,
-                            #  widget=CustomClearableFileInput
-    )
+        # image = forms.ImageField(label='Image', required=False, widget=ClearableFileInput())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
