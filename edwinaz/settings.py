a3af1ad6c36e4712ecc528b208efd934a30b9f2c
Padraig Dooley <padraigdooley@gmail.com>
2023-05-16 19:57:24 +0100

    add heroku site address to allowed hosts

--- edwinaz/settings.py
+++ edwinaz/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://edwinaz.herokuapp.com/', '127.0.0.1', 'localhost', 'https://*.gitpod.io/']
+ALLOWED_HOSTS = ['edwinaz.herokuapp.com', '127.0.0.1', 'localhost', 'https://*.gitpod.io/']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io/']
 
