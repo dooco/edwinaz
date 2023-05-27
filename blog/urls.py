5306f646ab409a94ea77df85cb5a0fd48d828242
Padraig Dooley <padraigdooley@gmail.com>
2023-05-27 21:50:38 +0100

    add articles to nav bar

--- blog/urls.py
+++ blog/urls.py
@@ -12,6 +12,4 @@ urlpatterns = [
     path('add/', CreatePost.as_view(), name='add_post'),
     path('edit/<slug:pk>/', EditPost.as_view(), name='edit_post'),
     path('delete/<slug:pk>/', DeletePost.as_view(), name='delete_post'),
-    # path('random/', RandomPostsView.as_view(), name='random_posts'),
-
 ]
